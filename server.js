b033324ec0ffc675387060f433bc080a52a0ec3b
JPage96 <justice.page1@pepsico.com>
2022-06-19 14:22:48 -0500

    added node packages

--- server.js
+++ server.js
@@ -1,1 +1,66 @@
-const express = require('express');
+// dependencies and express data parsing
+
+const fs = require('fs');
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const { notes } = require("./db/db");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+//unique id
+
+const generateUniqueId = require('generate-unique-id');
+
+//new note
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+      path.join(__dirname, './db/db.json'),
+      JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+  };  
+
+//route
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+  req.body.id = generateUniqueId();
+  const note = createNewNote(req.body, notes);
+  res.json(note);
+});
+    
+//delete bonus 
+
+app.delete('/api/notes/:id', (req, res) => {
+  const { id } = req.params;
+
+  const delNote = notes.findIndex(note => note.id ==id);
+
+  notes.splice(delNote, 1);
+  return res.send();
+});
+
+//listener
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT ${PORT}`);
+  });
